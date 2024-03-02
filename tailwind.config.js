85ae2444d6f9a5ec1070e03a4800d1e430266606
malakihwani <ihwanimalak@gmail.com>
2024-03-02 21:29:48 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+
+    extend: {
+      colors: {
+        primary: '#0369a1',
+        secondary: '#1e293b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
